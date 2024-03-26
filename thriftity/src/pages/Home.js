e9bb5842d1f56cf0f134c705208f692d18c2c671
anmolmittal13 <anmolmittal613@gmail.com>
2024-03-26 02:12:46 -0500

    add nav bar

--- thriftity/src/pages/Home.js
+++ thriftity/src/pages/Home.js
@@ -1,10 +1,22 @@
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Home = () => {
     return (
       <div>
-        <h2>Thriftity</h2>
-        <button><Link to="/Upload">Start Uploading</Link></button>
-        <button><Link to="/Offers">View Offers</Link></button>
+        <Row>
+          <Col> <h2>Thriftity</h2> </Col>
+        </Row>
+        
+        <div class='fixed-bottom nav-bar'>
+          <Row>
+            <Col> <button class='button'><Link to="/Upload">Start Uploading</Link></button> </Col>
+            <Col> <button class='button'><Link to="/Offers">View Offers</Link></button> </Col>
+          </Row>
+        </div>
       </div>
     );
   };
