1ed071e2f1c7d293492a5583adce3ad3dcfe1d00
Rachel Tan <racheltan@wireless-10-145-51-94.public.utexas.edu>
2024-04-11 18:19:44 -0500

    lucys changes

--- thriftify/src/pages/Offers.js
+++ thriftify/src/pages/Offers.js
@@ -180,4 +180,126 @@ const Offers = ({ clothesList, index, changeIndex }) => {
   );
 };
 
+// const Offers = ({ clothesList, index, changeIndex }) => {
+
+//   const calculatePriceRange = (itemIndex) => {
+//     if (clothesList.length === 0) {
+//       return [];
+//     }
+//     const currentItem = clothesList[itemIndex];
+//     const matchingStores = [];
+//     storesData.forEach((store) => {
+//       // console.log(`Checking store: ${store.name}`);
+//       const matchingBrand = store.brands.some((brand) => brand.value === currentItem.brand);
+//       if (matchingBrand) {
+//         const itemOffer = store.offers.find((offer) => offer.item === currentItem.category);
+//         const priceLow = itemOffer['price-low']
+//         const priceHigh = itemOffer['price-high']
+//         console.log(`Found matching offer at ${store.name}. Price range: ${priceLow} - ${priceHigh}`);
+//         if (!isNaN(priceLow) && !isNaN(priceHigh)) {
+//           matchingStores.push({ name: store.name, priceLow, priceHigh });
+//         }
+//       }
+//     });
+//     if (matchingStores.length === 0) {
+//       return [];
+//     }
+
+//     console.log(matchingStores);
+//     return matchingStores;
+//   };
+
+//   return (
+//     <div>
+//       <div className='title-container text-center'>
+//         <h2 className="offers-title mx-auto">offers</h2>
+//       </div>
+//       <div className="container offers-container">
+//         {clothesList && clothesList.length > 0 ? (
+//           <>
+//             <div className="col store-list">
+//               {clothesList.map((item, outerIndex) => (
+//                 <>
+//                   <div className='matches-header text-center'>{item.item}'s Offers</div>
+//                   <div class="row" style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
+//                     {calculatePriceRange(outerIndex).length !== 0 ? (
+//                       calculatePriceRange(outerIndex).map((store, innerIndex) => (
+//                         <div key={innerIndex} className="col store-item"  style={{ display: 'inline-block' }}>
+//                           <div className="row text-center">
+//                             <p className="store-name">{store.name}</p>
+//                             <p className="">{storesData[outerIndex].distance} miles</p>
+//                             <p className="">${store.priceLow} - ${store.priceHigh}</p>
+//                           </div>
+//                         </div>
+//                       ))
+//                     ) : (
+//                       <div className='no-match text-center'>
+//                         <img src={sadIcon} alt="Sad Icon" /><br />
+//                         No available matches for the current item.<br />
+//                       </div>
+//                     )}
+//                   </div>
+//                 </>
+//               ))}
+//             </div>
+
+
+
+//           </>
+//         ) : (
+//           <>
+//             <div className="container justify-content-center align-items-center text-center">
+//               <div className='row'>
+//                 <h3 className="no-offers">Upload your first item:</h3>
+//               </div>
+//               <div className='row'>
+//                 <div className='col'>
+//                   <button className='upload-btn'>
+//                     <Link to="/upload" className="btn btn-block">
+//                       <img className='upload' src={uploadIcon} alt="Upload Icon" />
+//                     </Link>
+//                   </button>
+//                 </div>
+//               </div>
+//             </div>
+
+//             <div className="container no-items-container d-flex justify-content-center align-items-center text-center">
+//               <div className="row">
+//                 <div className="col">
+//                   <h3 className="no-offers">No offers available.</h3>
+//                 </div>
+//               </div>
+//             </div>
+
+
+
+
+//           </>
+//         )}
+//         <Navbar />
+//       </div>
+//     </div>
+//   );
+// };
+
 export default Offers;
+
+/*
+<div key={index} className="store-item">
+                <div className="row">
+                  <div className="col-4">
+                    <p className="store-price">${store.priceLow} - ${store.priceHigh}</p>
+                  </div>
+                  <div className="col-8">
+                    <div className="row">
+                      <div className="col-12">
+                        <p className="store-name">{store.name}</p>
+                      </div>
+                      <div className="col-12">
+                        <p className="store-distance">{storesData[index].distance} miles</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+*/
